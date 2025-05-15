fe251bb67f465d3157397ef6667a7e0fc6b15f6d
RRomeroJr <117.rromero@gmail.com>
2025-05-15 19:56:09 -0400

    tri_split and ext_name added

--- rrjr_fm/__init__.py
+++ rrjr_fm/__init__.py
@@ -1,6 +1,9 @@
 import os as _os
 from typing import IO as _IO
 from typing import Any as _Any
+import re
+# with linux restrictions in mind. windows is more restrictive so this should still work
+ext_regex = re.compile(r'^\.([^/\0]+?)$')
 def sp_open(file, mode, **kwargs) -> _IO[_Any]:
     if (mode == "seq"):
         return open_avoid_overwrite(file, **kwargs)
@@ -34,17 +37,28 @@ def open_avoid_overwrite(filename: str, **kwargs):
         kwargs.get("closefd", True),
         kwargs.get("opener", None)
     )
+def tri_split(path) -> tuple[str, str, str]:
+    head, tail = _os.path.split(path)
+    return head, *_os.path.splitext(tail)
+def ext_name(ext) -> str:
+    """Takes in an extension, checks to see if it's valid then returns everything
+    after the '.'"""
+    match = re.match(ext_regex, ext)
+    if not match:
+        raise ValueError(f"Not invalid file extension: {ext}")
+    return match.group(1)
 if __name__ == "__main__":
-    import shutil as _shutil
-    try:
-        _shutil.rmtree("rrjr_hello_testfn_smile")
-    except FileNotFoundError:
-        pass
-    _os.makedirs("rrjr_hello_testfn_smile")
-    path = "rrjr_hello_testfn_smile/testThis_a_test_FFILLEEE.txt"
-    for x in range(10):
-        with sp_open(path, "seq", encoding="UTF-8", start = 7) as f:
-            f.write("all good")
-            print(f.name)
-    input("any to delete")
-    _shutil.rmtree("rrjr_hello_testfn_smile")
+    print(tri_split("a/b/c/d/f/someFile.txt"))
+    # import shutil as _shutil
+    # try:
+    #     _shutil.rmtree("rrjr_hello_testfn_smile")
+    # except FileNotFoundError:
+    #     pass
+    # _os.makedirs("rrjr_hello_testfn_smile")
+    # path = "rrjr_hello_testfn_smile/testThis_a_test_FFILLEEE.txt"
+    # for x in range(10):
+    #     with sp_open(path, "seq", encoding="UTF-8", start = 7) as f:
+    #         f.write("all good")
+    #         print(f.name)
+    # input("any to delete")
+    # _shutil.rmtree("rrjr_hello_testfn_smile")
