dda9ba6782488e260f76c2a2bd4a82279bff6b15
RRomeroJr <117.rromero@gmail.com>
2025-05-16 18:01:40 -0400

    ellipsis_truc added

--- rrjr_printing/__init__.py
+++ rrjr_printing/__init__.py
@@ -3,3 +3,11 @@ def pr_separate():
 
 def indent(spaces:int, inp:str):
     return "".join([" "] * spaces) + inp
+def ellipsis_truc(inp, max = 50, strip = True):
+    inp = str(inp)
+    if len(inp) <= max:
+        return inp
+    if strip:
+        return inp[:max].strip()+"..."
+    else:
+        return inp[:max]+"..."
